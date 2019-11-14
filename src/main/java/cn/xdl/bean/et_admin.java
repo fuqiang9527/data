b0a8b50d0d3d32922a653909bedd06bf6e7b4a9c
itxf <1443882747@qq.com>
2019-11-14 10:24:47 +0800

    添加hashcode

--- src/main/java/cn/xdl/bean/et_admin.java
+++ src/main/java/cn/xdl/bean/et_admin.java
@@ -35,6 +35,38 @@ public void setPassword(String password) {
 public String toString() {
 	return "et_admin [id=" + id + ", name=" + name + ", password=" + password + "]";
 }
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + id;
+	result = prime * result + ((name == null) ? 0 : name.hashCode());
+	result = prime * result + ((password == null) ? 0 : password.hashCode());
+	return result;
+}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	et_admin other = (et_admin) obj;
+	if (id != other.id)
+		return false;
+	if (name == null) {
+		if (other.name != null)
+			return false;
+	} else if (!name.equals(other.name))
+		return false;
+	if (password == null) {
+		if (other.password != null)
+			return false;
+	} else if (!password.equals(other.password))
+		return false;
+	return true;
+}
   
   
 }
